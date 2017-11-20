b7d8a63253ff264d823065994965e69778c5a014
Corey Menscher <corey@perceptivethings.com>
2017-11-20 12:15:15 -0500

    Added Google Maps API Key

--- priv/admin/admin-config.js
+++ priv/admin/admin-config.js
@@ -1,5 +1,5 @@
 /* Google API key, which has Google Maps enabled.
    set GOOGLE_MAPS_KEY environment variable to have this configured automatically */
-var GoogleMapsKey = "";
+var GoogleMapsKey = "AIzaSyDS1I6Ff9AIX0Ia5AlDPpVIy6Se2dihIiQ";
 
 /* end of file */
